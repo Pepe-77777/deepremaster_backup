248ded312053d87edf15b5125b7c467a2f4fefe7
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2023-03-09 14:16:29 -0300

    add old version

--- download_model.bat
+++ download_model.bat
@@ -2,4 +2,4 @@
 mkdir model
 cd model
 echo Downloading the RemasterNet (245MB)...
-curl http://iizuka.cs.tsukuba.ac.jp/data/remasternet.pth.tar --output remasternet.pth.tar
+curl https://web.archive.org/web/20210825063317/http://iizuka.cs.tsukuba.ac.jp/data/remasternet.pth.tar --output remasternet.pth.tar
